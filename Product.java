604e05149e51dc32d80e122393f1c860efaf1071
TaskiaJahan <161279478+TaskiaJahan@users.noreply.github.com>
2024-10-30 23:48:21 +0600

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,11 @@
+public class Product{
+    public static void main(String[]args)
+    {
+        System.out.println("id:2231081045");
+        System.out.println("title:iphone");
+        System.out.println("price:100k");
+        
+        
+        
+    }
+}
