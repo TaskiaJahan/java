c359ae843fa5608737777db61eeff575cd8652e2
TaskiaJahan <161279478+TaskiaJahan@users.noreply.github.com>
2024-11-04 00:54:38 +0600

    Update 15_Factorial.java

--- 15_Factorial.java
+++ 15_Factorial.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
-
-public class Factorial {
-
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int i, n, fact =1;
-		System.out.println("Enter a number : ");
-		n =input.nextInt();
-		for(i=1; i<=n; i++) {
-		fact = fact*i;
-		}
-		System.out.println("The factorial of n is : "+ fact);		
-	}
+public class factorialPrint{
+    public static void main(String[]args)
+    {
+        Scanner input=new Scanner(System.in);
+        
+        System.out.println("enter the  number:");
+        int num=input.nextInt();
+        int factorial=1;
+       for( int i=num;i>=1;i--)
+       { 
+           System.out.print(i+"*");
+           factorial=factorial*i;
+          
+       }
+        System.out.println("\n");
+        System.out.println(factorial);
+    }
 }
