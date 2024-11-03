8df7a41ee3a2abc08cc04a6307f1c3107a1431e9
TaskiaJahan <161279478+TaskiaJahan@users.noreply.github.com>
2024-11-03 15:15:05 +0600

    Add files via upload

---
+++ 14_Sum_problem.java
@@ -0,0 +1,16 @@
+// find sum of 1-10 using for loop
+public class Sum_problem{
+  public static void main(String[] args) {
+      
+      int i;
+      int sum=0;
+      for(i=0;i<=5;i++)
+      {
+          sum+=i;
+          
+      }
+      System.out.println("the sum is:"+sum);
+      
+    
+  }
+}
